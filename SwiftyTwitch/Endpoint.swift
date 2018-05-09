b3f9313b5cb326c94d4a621b0daa93ab1d74ddf2
Ryan Liang <xmomowan@gmail.com>
2018-05-09 21:47:38 +0800

    add Moya-like Endpoint solution

---
+++ SwiftyTwitch/Endpoint.swift
@@ -0,0 +1,36 @@
+//
+// Created by Ryan Liang on 5/18/18.
+// Copyright (c) 2018 Ryan Liang. All rights reserved.
+//
+
+import Foundation
+
+public typealias HTTPHeaders = [String: String]
+public typealias Parameters = [String: Any]
+
+public enum HTTPMethod: String {
+
+    case options = "OPTIONS"
+    case get = "GET"
+    case post = "POST"
+}
+
+public enum Action {
+
+    case plain
+
+    case parameters(parameters: Parameters)
+}
+
+protocol Endpoint {
+
+    var baseURL: URL { get }
+
+    var path: String { get }
+
+    var headers: HTTPHeaders? { get }
+
+    var action: Action { get }
+
+    var method: HTTPMethod { get }
+}
