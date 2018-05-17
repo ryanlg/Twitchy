8e43f5d01f7f29c0944ced630ed7701f3a1c96f6
Ryan Liang <xmomowan@gmail.com>
2018-05-18 01:32:06 +0800

    initial commit

---
+++ SwiftyTwitch/SwiftyTwitch.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyTwitch.h
+//  SwiftyTwitch
+//
+//  Created by Ryan Liang on 5/18/18.
+//  Copyright © 2018 Ryan Liang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyTwitch.
+FOUNDATION_EXPORT double SwiftyTwitchVersionNumber;
+
+//! Project version string for SwiftyTwitch.
+FOUNDATION_EXPORT const unsigned char SwiftyTwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyTwitch/PublicHeader.h>
+
+
